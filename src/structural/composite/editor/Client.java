f97e4edea8f0b5d62d81d2578e2193657ced93d5
OSKUDA <oscarstha@live.com>
2025-11-23 23:35:25 +0530

    add image editor example for composite structural pattern

---
+++ src/structural/composite/editor/Client.java
@@ -0,0 +1,32 @@
+package structural.composite.editor;
+
+import structural.composite.editor.shapes.Circle;
+import structural.composite.editor.shapes.CompoundShape;
+import structural.composite.editor.shapes.Dot;
+import structural.composite.editor.shapes.Rectangle;
+
+import java.awt.*;
+
+
+public class Client {
+    public static void main(String[] args) {
+        ImageEditor editor = new ImageEditor();
+
+        editor.loadShapes(
+                new Circle(10, 10, 10, Color.BLUE),
+
+                new CompoundShape(
+                        new Circle(110, 110, 50, Color.RED),
+                        new Dot(160, 160, Color.RED)
+                ),
+
+                new CompoundShape(
+                        new Rectangle(250, 250, 100, 100, Color.GREEN),
+                        new Dot(240, 240, Color.GREEN),
+                        new Dot(240, 360, Color.GREEN),
+                        new Dot(360, 360, Color.GREEN),
+                        new Dot(360, 240, Color.GREEN)
+                )
+        );
+    }
+}
